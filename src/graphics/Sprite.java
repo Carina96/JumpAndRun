999aa428ed2695fba167ed891fad0b1ac380d98c
Carina96 <45451715+Carina96@users.noreply.github.com>
2018-12-02 22:51:35 +0100

    Update Sprite.java

--- src/graphics/Sprite.java
+++ src/graphics/Sprite.java
@@ -1,5 +1,16 @@
 package graphics;
 
 public class Sprite {
+	
+	public Spritesheet sheet;
+	public BufferedImage image;
+	
+	public Sprite(Spritesheet sheet, int x,int y, int a,int b){
+		image = sheet.getSprite(x,y,a,b);
+	}
+	
+	public BufferedImage getBufferedImage(){
+		return image;
+	}
 
 }
